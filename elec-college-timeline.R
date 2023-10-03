11559f4526948896a2ec9c5dfe30b6f24ada64ad
mwhal012 <145363243+mwhal012@users.noreply.github.com>
2023-10-03 19:35:52 -0400

    Add files via upload

--- elec-college-timeline.R
+++ elec-college-timeline.R
@@ -23,7 +23,7 @@ means = pred |>
 result = tibble(
   votes_timeline = c(232, 232),
   dates = c(
-    ymd("2016-11-08"),
+    ymd("2016-11-07"),
     ymd("2016-11-09")
   )
 )
@@ -35,13 +35,7 @@ means = means |>
   slice_tail(n = nrow(pred))
 fiftypctpo = 270 # 538/2 + 1
 pred |>
-  ggplot() + geom_segment(
-    x = earliestdate - days(2),
-    xend = ymd("2016-11-07") + days(2),
-    y = fiftypctpo,
-    yend = fiftypctpo,
-    linetype = "11"
-  ) + geom_ribbon(
+  ggplot() + geom_ribbon(
     data = means,
     mapping = aes(
       x = dates,
@@ -57,20 +51,19 @@ pred |>
       ymax = fiftypctpo,
       ymin = vapply(X = means_timeline, FUN = min, FUN.VALUE = 0, fiftypctpo)
     ),
-    alpha = 0.7,
-    fill = "red"
-  ) + labs(
-    title = "Predicted Results of Electoral College",
-    subtitle = "assuming faithful electors"
-  ) + xlab("Polling Date") + ylab("Electoral votes for Hillary Clinton") + geom_segment(
+    fill = "red",
+    alpha = 0.7
+  ) + labs(title = "Predicted Electoral Votes Cast for Hillary Clinton") + xlab("Polling Date") + ylab("") + geom_ribbon(
     data = result,
     mapping = aes(
-      x = ymd("2016-11-08"),
-      xend = ymd("2016-11-08"),
-      y = 232,
-      yend = 270
+      x = dates,
+      ymin = 232,
+      ymax = 270
     ),
-    colour = "red",
-    alpha = 0.7,
-    linewidth = 2
-  ) + scale_y_continuous(position = "right", breaks = c(230, 250, 270, 290, 310, 330, 350)) + scale_x_date(date_breaks = "2 weeks") + theme_classic() + theme(axis.text.x = element_text(angle = 45, vjust = -0.001))
+    fill = "red",
+    alpha = 0.7
+  ) + scale_y_continuous(
+    position = "right",
+    breaks = c(230, 250, 270, 290, 310, 330, 350),
+    limits = c(230, 350)
+  ) + scale_x_date(limits = c(ymd("2016-08-01"), ymd("2016-11-09"))) + theme_classic() + theme(plot.title = element_text(hjust = 0.5), axis.line.x = element_blank(), axis.line.y = element_blank(), plot.margin = unit(c(0.75,0.75,0.75,0.5), "cm"))
